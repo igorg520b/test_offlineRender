a439bbdfbe297a415dd52faf7e9e4cadb54654ac
Igor Gribanov <igor1215@gmail.com>
2024-11-25 16:04:05 +0900

    3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h> // GLFW is still used for OpenGL context management
 #include <cuda_runtime.h>
 #include <cuda_gl_interop.h>
 #include <iostream>
@@ -10,14 +10,13 @@
 #include <cstring>
 #include "stb_image_write.h"
 
-
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 800;
+const int IMAGE_WIDTH = 800;
+const int IMAGE_HEIGHT = 800;
 const int NUM_POINTS = 1000; // Number of points to render
-const double POINT_RADIUS = 10.0; // Radius of the circles in pixels
+const float POINT_RADIUS = 10.0f; // Radius of the circles in pixels
 
 struct Point {
-    double x, y;
+    float x, y;
 };
 
 struct RGB {
@@ -29,15 +28,13 @@ RGB *cudaColors;
 cudaGraphicsResource *cudaVbo, *cudaColorVbo;
 GLuint vbo, colorVbo, fbo, renderTexture;
 
-// Function to create random points
 void createRandomPoints(std::vector<Point>& points, int numPoints) {
     for (int i = 0; i < numPoints; ++i) {
-        points[i].x = -1.0 + 2.0 * ((double)rand() / RAND_MAX); // Random x in [-1, 1]
-        points[i].y = -1.0 + 2.0 * ((double)rand() / RAND_MAX); // Random y in [-1, 1]
+        points[i].x = -1.0f + 2.0f * ((float)rand() / RAND_MAX); // Random x in [-1, 1]
+        points[i].y = -1.0f + 2.0f * ((float)rand() / RAND_MAX); // Random y in [-1, 1]
     }
 }
 
-// Custom scalar-to-RGB conversion function
 RGB scalarToRGB(float scalar) {
     unsigned char r = (unsigned char)(255 * scalar);
     unsigned char g = 0;
@@ -45,7 +42,6 @@ RGB scalarToRGB(float scalar) {
     return {r, g, b};
 }
 
-// Function to create random colors
 void createRandomColors(std::vector<RGB>& colors, int numPoints) {
     for (int i = 0; i < numPoints; ++i) {
         float scalar = (float)rand() / RAND_MAX; // Random scalar in [0, 1]
@@ -53,50 +49,41 @@ void createRandomColors(std::vector<RGB>& colors, int numPoints) {
     }
 }
 
-// Initialize OpenGL VBOs
 void initVbos(int numPoints) {
-    // Points VBO
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, numPoints * sizeof(Point), nullptr, GL_DYNAMIC_DRAW);
 
-    // Colors VBO
     glGenBuffers(1, &colorVbo);
     glBindBuffer(GL_ARRAY_BUFFER, colorVbo);
     glBufferData(GL_ARRAY_BUFFER, numPoints * sizeof(RGB), nullptr, GL_DYNAMIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    // Register VBOs with CUDA
     cudaGraphicsGLRegisterBuffer(&cudaVbo, vbo, cudaGraphicsMapFlagsWriteDiscard);
     cudaGraphicsGLRegisterBuffer(&cudaColorVbo, colorVbo, cudaGraphicsMapFlagsWriteDiscard);
 }
 
-// Initialize the framebuffer for off-screen rendering
 void initFramebuffer() {
     glGenFramebuffers(1, &fbo);
     glBindFramebuffer(GL_FRAMEBUFFER, fbo);
 
-    // Create texture to store the framebuffer image
     glGenTextures(1, &renderTexture);
     glBindTexture(GL_TEXTURE_2D, renderTexture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WINDOW_WIDTH, WINDOW_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, IMAGE_WIDTH, IMAGE_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderTexture, 0);
 
-    // Check if framebuffer is complete
     if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-        std::cerr << "Failed to create framebuffer!" << std::endl;
+        std::cerr << "Framebuffer is incomplete!" << std::endl;
         exit(-1);
     }
 
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
-// Copy host data to CUDA buffers
 void copyHostToDevice(const std::vector<Point>& points, const std::vector<RGB>& colors, int numPoints) {
-    // Map OpenGL buffers for CUDA
     Point *mappedPoints = nullptr;
     RGB *mappedColors = nullptr;
 
@@ -107,19 +94,16 @@ void copyHostToDevice(const std::vector<Point>& points, const std::vector<RGB>& 
     cudaGraphicsResourceGetMappedPointer((void **)&mappedPoints, &numBytes, cudaVbo);
     cudaGraphicsResourceGetMappedPointer((void **)&mappedColors, &numBytes, cudaColorVbo);
 
-    // Copy data to CUDA buffers
     cudaMemcpy(mappedPoints, points.data(), numPoints * sizeof(Point), cudaMemcpyHostToDevice);
     cudaMemcpy(mappedColors, colors.data(), numPoints * sizeof(RGB), cudaMemcpyHostToDevice);
 
-    // Unmap resources
     cudaGraphicsUnmapResources(1, &cudaVbo, 0);
     cudaGraphicsUnmapResources(1, &cudaColorVbo, 0);
 }
 
-// Render points with colors
 void render(int numPoints) {
     glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glViewport(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
 
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -127,7 +111,7 @@ void render(int numPoints) {
     glEnableClientState(GL_COLOR_ARRAY);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glVertexPointer(2, GL_DOUBLE, sizeof(Point), (void *)0);
+    glVertexPointer(2, GL_FLOAT, sizeof(Point), (void *)0);
 
     glBindBuffer(GL_ARRAY_BUFFER, colorVbo);
     glColorPointer(3, GL_UNSIGNED_BYTE, sizeof(RGB), (void *)0);
@@ -147,22 +131,20 @@ void render(int numPoints) {
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
-// Save the rendered image to PNG
 void saveFramebufferToPNG(const char *filename) {
-    std::vector<unsigned char> pixels(WINDOW_WIDTH * WINDOW_HEIGHT * 3);
+    std::vector<unsigned char> pixels(IMAGE_WIDTH * IMAGE_HEIGHT * 3);
     glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-    glReadPixels(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, pixels.data());
+    glReadPixels(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, pixels.data());
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-    // Flip the image vertically because OpenGL's origin is bottom-left
-    std::vector<unsigned char> flippedPixels(WINDOW_WIDTH * WINDOW_HEIGHT * 3);
-    for (int y = 0; y < WINDOW_HEIGHT; ++y) {
-        std::memcpy(&flippedPixels[y * WINDOW_WIDTH * 3],
-                    &pixels[(WINDOW_HEIGHT - 1 - y) * WINDOW_WIDTH * 3],
-                    WINDOW_WIDTH * 3);
+    std::vector<unsigned char> flippedPixels(IMAGE_WIDTH * IMAGE_HEIGHT * 3);
+    for (int y = 0; y < IMAGE_HEIGHT; ++y) {
+        std::memcpy(&flippedPixels[y * IMAGE_WIDTH * 3],
+                    &pixels[(IMAGE_HEIGHT - 1 - y) * IMAGE_WIDTH * 3],
+                    IMAGE_WIDTH * 3);
     }
 
-    if (stbi_write_png(filename, WINDOW_WIDTH, WINDOW_HEIGHT, 3, flippedPixels.data(), WINDOW_WIDTH * 3)) {
+    if (stbi_write_png(filename, IMAGE_WIDTH, IMAGE_HEIGHT, 3, flippedPixels.data(), IMAGE_WIDTH * 3)) {
         std::cout << "Image saved to " << filename << std::endl;
     } else {
         std::cerr << "Failed to save image!" << std::endl;
@@ -175,9 +157,10 @@ int main() {
         return -1;
     }
 
-    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "CUDA + OpenGL (Save to PNG)", nullptr, nullptr);
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // Invisible window
+    GLFWwindow *window = glfwCreateWindow(IMAGE_WIDTH, IMAGE_HEIGHT, "Off-Screen", nullptr, nullptr);
     if (!window) {
-        std::cerr << "Failed to create GLFW window\n";
+        std::cerr << "Failed to create GLFW context\n";
         glfwTerminate();
         return -1;
     }
@@ -187,8 +170,6 @@ int main() {
         std::cerr << "Failed to initialize GLEW\n";
         return -1;
     }
-
-    glEnable(GL_MULTISAMPLE);
 
     std::vector<Point> points(NUM_POINTS);
     createRandomPoints(points, NUM_POINTS);
@@ -214,3 +195,4 @@ int main() {
     glfwTerminate();
     return 0;
 }
+
