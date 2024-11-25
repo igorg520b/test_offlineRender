8a5a8ece7eac78576282ff205f997fe481887873
Igor Gribanov <igor1215@gmail.com>
2024-11-25 17:21:59 +0900

    3

--- main.cpp
+++ main.cpp
@@ -1,150 +1,61 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-
-#include <GL/glew.h>
-#include <GLFW/glfw3.h> // GLFW is still used for OpenGL context management
-#include <cuda_runtime.h>
-#include <cuda_gl_interop.h>
+#include "stb_image_write.h"
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <GL/gl.h>
 #include <iostream>
 #include <vector>
-#include <cstdlib> // For rand()
-#include <cstring>
-#include "stb_image_write.h"
+#include <cstring> // For memcpy
 
 const int IMAGE_WIDTH = 800;
 const int IMAGE_HEIGHT = 800;
-const int NUM_POINTS = 1000; // Number of points to render
-const float POINT_RADIUS = 10.0f; // Radius of the circles in pixels
 
-struct Point {
-    float x, y;
-};
-
-struct RGB {
-    unsigned char r, g, b;
-};
-
-Point *cudaPoints;
-RGB *cudaColors;
-cudaGraphicsResource *cudaVbo, *cudaColorVbo;
-GLuint vbo, colorVbo, fbo, renderTexture;
-
-void createRandomPoints(std::vector<Point>& points, int numPoints) {
-    for (int i = 0; i < numPoints; ++i) {
-        points[i].x = -1.0f + 2.0f * ((float)rand() / RAND_MAX); // Random x in [-1, 1]
-        points[i].y = -1.0f + 2.0f * ((float)rand() / RAND_MAX); // Random y in [-1, 1]
-    }
-}
-
-RGB scalarToRGB(float scalar) {
-    unsigned char r = (unsigned char)(255 * scalar);
-    unsigned char g = 0;
-    unsigned char b = (unsigned char)(255 * (1.0f - scalar));
-    return {r, g, b};
-}
-
-void createRandomColors(std::vector<RGB>& colors, int numPoints) {
-    for (int i = 0; i < numPoints; ++i) {
-        float scalar = (float)rand() / RAND_MAX; // Random scalar in [0, 1]
-        colors[i] = scalarToRGB(scalar);
-    }
-}
-
-void initVbos(int numPoints) {
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, numPoints * sizeof(Point), nullptr, GL_DYNAMIC_DRAW);
-
-    glGenBuffers(1, &colorVbo);
-    glBindBuffer(GL_ARRAY_BUFFER, colorVbo);
-    glBufferData(GL_ARRAY_BUFFER, numPoints * sizeof(RGB), nullptr, GL_DYNAMIC_DRAW);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    cudaGraphicsGLRegisterBuffer(&cudaVbo, vbo, cudaGraphicsMapFlagsWriteDiscard);
-    cudaGraphicsGLRegisterBuffer(&cudaColorVbo, colorVbo, cudaGraphicsMapFlagsWriteDiscard);
-}
-
-void initFramebuffer() {
-    glGenFramebuffers(1, &fbo);
-    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-
-    glGenTextures(1, &renderTexture);
-    glBindTexture(GL_TEXTURE_2D, renderTexture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, IMAGE_WIDTH, IMAGE_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderTexture, 0);
-
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-        std::cerr << "Framebuffer is incomplete!" << std::endl;
-        exit(-1);
-    }
-
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-}
-
-void copyHostToDevice(const std::vector<Point>& points, const std::vector<RGB>& colors, int numPoints) {
-    Point *mappedPoints = nullptr;
-    RGB *mappedColors = nullptr;
-
-    cudaGraphicsMapResources(1, &cudaVbo, 0);
-    cudaGraphicsMapResources(1, &cudaColorVbo, 0);
-
-    size_t numBytes;
-    cudaGraphicsResourceGetMappedPointer((void **)&mappedPoints, &numBytes, cudaVbo);
-    cudaGraphicsResourceGetMappedPointer((void **)&mappedColors, &numBytes, cudaColorVbo);
-
-    cudaMemcpy(mappedPoints, points.data(), numPoints * sizeof(Point), cudaMemcpyHostToDevice);
-    cudaMemcpy(mappedColors, colors.data(), numPoints * sizeof(RGB), cudaMemcpyHostToDevice);
-
-    cudaGraphicsUnmapResources(1, &cudaVbo, 0);
-    cudaGraphicsUnmapResources(1, &cudaColorVbo, 0);
-}
-
-void render(int numPoints) {
-    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-    glViewport(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
-
+void renderScene() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // White background
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glVertexPointer(2, GL_FLOAT, sizeof(Point), (void *)0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, colorVbo);
-    glColorPointer(3, GL_UNSIGNED_BYTE, sizeof(RGB), (void *)0);
-
-    glEnable(GL_POINT_SMOOTH);
+    // Enable blending
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    glPointSize(POINT_RADIUS);
-    glDrawArrays(GL_POINTS, 0, numPoints);
+    // Enable smooth point rendering
+    glEnable(GL_POINT_SMOOTH);
 
-    glDisable(GL_BLEND);
-    glDisable(GL_POINT_SMOOTH);
+    glPointSize(10.0f); // Set point size
+    glBegin(GL_POINTS);
+    for (int i = 0; i < 1000; ++i) {
+        float x = -1.0f + 2.0f * ((float)rand() / RAND_MAX); // Random x in [-1, 1]
+        float y = -1.0f + 2.0f * ((float)rand() / RAND_MAX); // Random y in [-1, 1]
+        glColor4f((float)rand() / RAND_MAX,                // Random red
+                  (float)rand() / RAND_MAX,                // Random green
+                  (float)rand() / RAND_MAX,                // Random blue
+                  0.7f);                                   // Semi-transparent alpha
+        glVertex2f(x, y);
+    }
+    glEnd();
 
-    glDisableClientState(GL_COLOR_ARRAY);
-    glDisableClientState(GL_VERTEX_ARRAY);
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glFlush();
 }
 
-void saveFramebufferToPNG(const char *filename) {
-    std::vector<unsigned char> pixels(IMAGE_WIDTH * IMAGE_HEIGHT * 3);
-    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-    glReadPixels(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, pixels.data());
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-    std::vector<unsigned char> flippedPixels(IMAGE_WIDTH * IMAGE_HEIGHT * 3);
-    for (int y = 0; y < IMAGE_HEIGHT; ++y) {
-        std::memcpy(&flippedPixels[y * IMAGE_WIDTH * 3],
-                    &pixels[(IMAGE_HEIGHT - 1 - y) * IMAGE_WIDTH * 3],
-                    IMAGE_WIDTH * 3);
+void saveJPG(const char *filename, const unsigned char *buffer, int width, int height) {
+    // Convert RGBA to RGB by discarding the alpha channel
+    std::vector<unsigned char> rgbBuffer(width * height * 3);
+    for (int i = 0; i < width * height; ++i) {
+        rgbBuffer[i * 3 + 0] = buffer[i * 4 + 0]; // R
+        rgbBuffer[i * 3 + 1] = buffer[i * 4 + 1]; // G
+        rgbBuffer[i * 3 + 2] = buffer[i * 4 + 2]; // B
     }
 
-    if (stbi_write_png(filename, IMAGE_WIDTH, IMAGE_HEIGHT, 3, flippedPixels.data(), IMAGE_WIDTH * 3)) {
+    // Flip the image vertically because OpenGL's origin is bottom-left
+    std::vector<unsigned char> flippedBuffer(width * height * 3);
+    for (int y = 0; y < height; ++y) {
+        std::memcpy(&flippedBuffer[y * width * 3],
+                    &rgbBuffer[(height - 1 - y) * width * 3],
+                    width * 3);
+    }
+
+    // Save as JPG
+    if (stbi_write_jpg(filename, width, height, 3, flippedBuffer.data(), 100)) {
         std::cout << "Image saved to " << filename << std::endl;
     } else {
         std::cerr << "Failed to save image!" << std::endl;
@@ -152,47 +63,88 @@ void saveFramebufferToPNG(const char *filename) {
 }
 
 int main() {
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW\n";
+    // Initialize EGL
+    EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    if (display == EGL_NO_DISPLAY) {
+        std::cerr << "Failed to get EGL display!" << std::endl;
         return -1;
     }
 
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // Invisible window
-    GLFWwindow *window = glfwCreateWindow(IMAGE_WIDTH, IMAGE_HEIGHT, "Off-Screen", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW context\n";
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to initialize GLEW\n";
+    if (!eglInitialize(display, nullptr, nullptr)) {
+        std::cerr << "Failed to initialize EGL!" << std::endl;
         return -1;
     }
 
-    std::vector<Point> points(NUM_POINTS);
-    createRandomPoints(points, NUM_POINTS);
+    // Configure EGL
+    const EGLint configAttribs[] = {
+        EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+        EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+        EGL_RED_SIZE, 8,
+        EGL_GREEN_SIZE, 8,
+        EGL_BLUE_SIZE, 8,
+        EGL_ALPHA_SIZE, 8,
+        EGL_DEPTH_SIZE, 24,
+        EGL_NONE
+    };
 
-    std::vector<RGB> colors(NUM_POINTS);
-    createRandomColors(colors, NUM_POINTS);
+    EGLConfig config;
+    EGLint numConfigs;
+    if (!eglChooseConfig(display, configAttribs, &config, 1, &numConfigs) || numConfigs < 1) {
+        std::cerr << "Failed to choose EGL config!" << std::endl;
+        return -1;
+    }
 
-    initVbos(NUM_POINTS);
-    initFramebuffer();
-    copyHostToDevice(points, colors, NUM_POINTS);
+    // Create an off-screen surface
+    const EGLint pbufferAttribs[] = {
+        EGL_WIDTH, IMAGE_WIDTH,
+        EGL_HEIGHT, IMAGE_HEIGHT,
+        EGL_NONE
+    };
+    EGLSurface surface = eglCreatePbufferSurface(display, config, pbufferAttribs);
+    if (surface == EGL_NO_SURFACE) {
+        std::cerr << "Failed to create EGL surface!" << std::endl;
+        return -1;
+    }
 
-    render(NUM_POINTS);
-    saveFramebufferToPNG("output.png");
+    // Bind OpenGL API
+    if (!eglBindAPI(EGL_OPENGL_API)) {
+        std::cerr << "Failed to bind OpenGL API!" << std::endl;
+        return -1;
+    }
 
-    cudaGraphicsUnregisterResource(cudaVbo);
-    cudaGraphicsUnregisterResource(cudaColorVbo);
-    glDeleteBuffers(1, &vbo);
-    glDeleteBuffers(1, &colorVbo);
-    glDeleteTextures(1, &renderTexture);
-    glDeleteFramebuffers(1, &fbo);
+    // Create an EGL context
+    const EGLint contextAttribs[] = {EGL_NONE};
+    EGLContext context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs);
+    if (context == EGL_NO_CONTEXT) {
+        std::cerr << "Failed to create EGL context!" << std::endl;
+        return -1;
+    }
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    // Make the context current
+    if (!eglMakeCurrent(display, surface, surface, context)) {
+        std::cerr << "Failed to make EGL context current!" << std::endl;
+        return -1;
+    }
+
+    // Allocate memory for the framebuffer
+    std::vector<unsigned char> framebuffer(IMAGE_WIDTH * IMAGE_HEIGHT * 4); // RGBA format
+
+    // Set the viewport
+    glViewport(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    // Render the scene
+    renderScene();
+
+    // Read pixels from the framebuffer
+    glReadPixels(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT, GL_RGBA, GL_UNSIGNED_BYTE, framebuffer.data());
+
+    // Save the framebuffer to a JPG file
+    saveJPG("output.jpg", framebuffer.data(), IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    // Clean up
+    eglDestroySurface(display, surface);
+    eglDestroyContext(display, context);
+    eglTerminate(display);
+
     return 0;
 }
-
